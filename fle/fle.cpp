c82d9ab2e886dd05213664efee045439bfd73350
Daria <zerlovad@gmail.com>
2025-06-18 12:07:35 +0800

    ADDED sort2 newDir

--- fle/fle.cpp
+++ fle/fle.cpp
@@ -18,4 +18,45 @@ struct inf {
     long long size;
     string time;
 };
-// ловаlkxjfdlkxdjfl
+
+bool sort2(const inf& a, const inf& b) {
+    if (a.flag != b.flag) return a.flag > b.flag;
+    return a.name < b.name;
+}
+
+
+vector<inf> openDir(const path& path) {
+    vector<inf> data;
+    try {
+        for (const auto& iter : directory_iterator(path)) {
+            inf info;
+            info.name = iter.path().filename().string();
+            info.flag = iter.is_directory();
+            if (info.flag)
+            {
+                info.size = 0;
+            }
+            else
+            {
+                info.size = iter.file_size();
+            }
+
+            auto ftime = iter.last_write_time();
+            auto stime = chrono::clock_cast<chrono::system_clock>(ftime);
+            time_t timet = chrono::system_clock::to_time_t(stime);
+
+            char timeStr[100];
+            struct tm time;
+            localtime_s(&time, &timet);
+            strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", &time);
+            info.time = timeStr;
+
+            data.push_back(info);
+        }
+        sort(data.begin(), data.end(), sort2);
+    }
+    catch (const exception& e) {
+        cerr << "Ошибка: " << e.what() << "\n";
+    }
+    return data;
+}
