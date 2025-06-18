85e3446b186d527b9b5e57c67a52dc9c2c4f6623
Daria <zerlovad@gmail.com>
2025-06-18 11:23:29 +0800

    Initial commit

---
+++ fle/fle.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <conio.h>
+#include <chrono>
+#include <iomanip>
+
+using namespace std;
+
+using namespace filesystem;
+
+struct inf {
+    string name;
+    bool flag;
+    long long size;
+    string time;
+};
+// ловатжло
